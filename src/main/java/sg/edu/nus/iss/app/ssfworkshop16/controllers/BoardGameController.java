db7bf332f7433e56b5176502cd14ce5f30096cf3
Wee Seng <wee.bscl@gmail.com>
2023-01-17 14:01:03 +0800

    fixed wrong import

--- src/main/java/sg/edu/nus/iss/app/ssfworkshop16/controllers/BoardGameController.java
+++ src/main/java/sg/edu/nus/iss/app/ssfworkshop16/controllers/BoardGameController.java
@@ -1,7 +1,7 @@
-package src.main.java.sg.edu.nus.iss.app.ssfworkshop16.controllers;
+package sg.edu.nus.iss.app.ssfworkshop16.controllers;
 
-import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import sg.edu.nus.iss.app.ssfworkshop16.models.Mastermind;
+
 
 @RestController
 @RequestMapping(path = "/api/boardgame", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
